5b63c7602ec65e2a313ff72ac0c31840201be607
wakamyc701 <wakamyc701@gmail.com>
2024-10-28 15:46:16 +0900

    引き続きwork19 書き込み内容表示

--- htdocs/work19/work19.php
+++ htdocs/work19/work19.php
@@ -16,27 +16,43 @@
         <title>WORK19</title>
     </head>
     <body>
-        <?php
-            // 入力情報不足時の処理
-        ?>
         <form method="post">
             タイトル<input type="text" name="post_title"><br>
             書き込み内容<input type="text" name="post_text"><br>
             <input type="submit" value="送信">
+
+            <?php
+                $fp = fopen("file_write.txt","a+");
+
+                fwrite($fp, $post_title."\t".$post_text."\n");  // 書き込み処理、ファイルポインタは末尾
+
+
+                // ファイル内容の読み込みと表示
+
+                fseek($fp, 0);  //ファイルポインタを先頭に
+
+                $l_array = array();
+                $i = 0;
+                while ($line = fgets($fp)) {
+                    array_push($l_array, $line);
+                    //print "$line<br>";
+                    $i++;
+                }
+
+                print "<p>";
+                for ($j = $i - 1 ; $j >= 0; $j--) {
+                    print "$l_array[$j]<br>";
+                }
+                print "</p>";
+
+                // フォーム入力内容の書き込み
+                // 入力内容不足の場合は書き込まない処理←入力内容不足の判定条件は？
+
+                //fwrite($fp, $post_title."：".$post_text."\n");  // 書き込み処理
+
+
+                fclose($fp);
+            ?>
         </form>
-
-        <?php
-            $fp = fopen("file_write.txt","a+");
-            // フォーム入力内容の書き込み
-            // 入力内容不足の場合は書き込まない処理←入力内容不足の判定条件は？
-
-            fwrite($fp, $post_title.'：'.$post_text."\n");  // 書き込み処理
-
-
-            // ファイル内容の表示
-
-            fclose($fp);
-        ?>
-
     </body>
 </html>
