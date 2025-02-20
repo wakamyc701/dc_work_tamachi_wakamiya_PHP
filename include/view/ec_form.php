2ae7a7d1b9c7f9e6d4e193c9822380421d13f599
wakamyc701 <wakamyc701@gmail.com>
2025-02-20 15:17:10 +0900

    最終課題　inputの修正完了

--- include/view/ec_form.php
+++ include/view/ec_form.php
@@ -2,6 +2,6 @@
     <p>ユーザー名
     <input type="text" name="user_name" pattern="^\w{5,}$" title="「5文字以上」かつ「半角英数字とアンダースコア」のみ有効です"></p>
     <p>パスワード
-    <input type="text" name="user_password" pattern="^\w{8,}$" title="「8文字以上」かつ「半角英数字とアンダースコア」のみ有効です"></p>
+    <input type="password" name="user_password" pattern="^\w{8,}$" title="「8文字以上」かつ「半角英数字とアンダースコア」のみ有効です"></p>
     <button class="form_btn"><?php echo $btn_title; ?></button>
 </form>
